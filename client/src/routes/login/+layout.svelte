959234d5183dacdc433fa3c48861a9a836d7afc2
carlsylvan <carlsylvan@gmail.com>
2024-07-04 11:12:20 +0200

    layout added to login page

---
+++ client/src/routes/login/+layout.svelte
@@ -0,0 +1,11 @@
+<div class="main-content">
+    <slot></slot>
+</div>
+
+<style>
+    .main-content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
